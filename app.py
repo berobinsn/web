4c912391f83d2dea06169326ba8b4be8e60f91ba
berobinsn <blakeeverettrobinson@gmail.com>
2023-10-11 20:02:12 -0500

    fourth commit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = '1lgf8absm30yh31lasdfbt40311ubn'
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
