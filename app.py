c9d233b46f2d5b2d537a7657aef1512f2cebf91a
berobinsn <blakeeverettrobinson@gmail.com>
2023-10-11 20:20:03 -0500

    sixth commit

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ app.secret_key = '1lgf8absm30yh31lasdfbt40311ubn'
 def index():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/mtg_projects')
+def mtg_projects():
+    return render_template("mtg_projects.html")
+
+@app.route('/other_projects')
+def other_projects():
+    return render_template("other_projects.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 if __name__ == '__main__':
     app.run()
